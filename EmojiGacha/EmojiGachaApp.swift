d80d7f88ed21067c10b3c893fd85f63646f133fc
gurrium <gurrium@hatena.ne.jp>
2021-08-07 12:35:18 +0900

    Initial Commit

---
+++ EmojiGacha/EmojiGachaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiGachaApp.swift
+//  EmojiGacha
+//
+//  Created by gurrium on 2021/08/07.
+//
+
+import SwiftUI
+
+@main
+struct EmojiGachaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8905b1ac60b4656716dd34432c8333a63ec7d097
gurrium <gurrium@hatena.ne.jp>
2021-08-08 15:54:31 +0900

    🛠 enum Emojiを作るスクリプトを修正した

--- scripts/generator.rb
+++ scripts/generator.rb
@@ -1,43 +1,32 @@
+require 'nokogiri'
+
 class Generator
-  def initialize(input_filename, output_filename)
+  def initialize(output_filename)
     @emoji_list = []
-    @input_filename = input_filename
     @output_filename = output_filename
-    # Usage:
-    # [
-    #   {
-    #     code_points: [0x231a],
-    #     description: 'watch1',
-    #   },
-    #   {
-    #     code_points: [0x231b],
-    #     description: 'watch2',
-    #   },
-    # ]
   end
 
-  def main
+  def generate
     build_emoji_list
-
     generate_enum_emoji
   end
 
   private def build_emoji_list
-    File.open(@input_filename) do |f|
-      f.each_line do |line|
-        next unless line.match?(/^[\h\.]+ /)
+    File.open('html/emoji-list.html') do |f|
+      doc = Nokogiri::HTML(f)
+      parse(doc).each { |e| append_to_emoji_list(e) }
+    end
+    File.open('html/full-emoji-modifiers.html') do |f|
+      doc = Nokogiri::HTML(f)
+      parse(doc).each { |e| append_to_emoji_list(e) }
+    end
+  end
 
-        raw_code_points, _, raw_description = line.split(';').map(&:strip)
-        description = raw_description.split('#').first.strip
-        endpoints = raw_code_points.split('..')
-        if endpoints.count == 2
-          (endpoints[0].to_i(16)...endpoints[1].to_i(16)).each_with_index do |code_point, index|
-            append_to_emoji_list(code_points: [code_point], description: description + "_#{index}")
-          end
-        else
-          append_to_emoji_list(code_points: raw_code_points.split.map { |code_point| code_point.to_i(16) }, description: description)
-        end
-      end
+  private def parse(doc)
+    doc.xpath("//tr/td[@class='code']/parent::tr").xpath("td[@class='code'] | td[@class='name'][1]").each_slice(2).map do |code_element, name_element|
+      code_points = code_element.xpath("a/text()")[0].text.split.map { |code_point| code_point.delete_prefix('U+').to_i(16) }
+      description = name_element.xpath('text()')[0].text
+      { code_points: code_points, description: description }
     end
   end
 
@@ -52,6 +41,10 @@ class Generator
     File.open(@output_filename, 'w') do |f|
       f.puts <<~DEF
         enum Emoji: CaseIterable {
+            static func random() -> Emoji {
+                Emoji.allCases.randomElement()!
+            }
+
             case #{cases}
 
             var image: String {
@@ -64,14 +57,14 @@ class Generator
                 switch self {
                 #{unicodeScalars}
                 default:
-                    fatalError()
+                    preconditionFailure("EmojiGacha: Unknown emoji")
                 }
             }
             var description: String {
                 switch self {
                 #{descriptions}
                 default:
-                    fatalError()
+                    preconditionFailure("EmojiGacha: Unknown emoji")
                 }
             }
         }
@@ -80,15 +73,29 @@ class Generator
   end
 
   private def case_name(string)
-    snakecased = string
+    return '`guard`' if string == 'guard'
+
+    partially_sanitized = string
       .gsub('&', 'and')
       .gsub('1st', 'first')
+      .gsub('2nd', 'second')
+      .gsub('3rd', 'third')
       .gsub('*', 'asterisk')
-      .gsub('\x{23}', 'hash')
-      .gsub(/[^0-9a-zA-Z]/, '_')
+      .gsub('#', 'hash')
+
+    sanitized = ''
+    if partially_sanitized.match?(/flag:/)
+      sanitized = partially_sanitized
+        .gsub(/[’: \.\(\)\-]/, '_')
+    else
+      sanitized = partially_sanitized
+        .gsub(/[^0-9a-zA-Z]/, '_')
+    end
+
+    snakecased = sanitized
       .gsub(/_+/, '_') 
       .downcase
-    snakecased = snakecased.split('_')
+      .split('_')
     [snakecased[0], *snakecased[1..-1].map(&:capitalize)].join
   end
 
@@ -101,16 +108,16 @@ class Generator
       <<~CASE.chomp
         case .#{case_name(emoji[:description])}: return #{emoji[:code_points]};
       CASE
-    end.join("\n")
+    end.join("\n" + ' ' * 8)
   end
 
   private def descriptions
     @emoji_list.map do |emoji|
       <<~CASE.chomp
-        case .#{case_name(emoji[:description])}: return "#{emoji[:description].gsub('\x', '\\\\\\x')}";
+        case .#{case_name(emoji[:description])}: return "#{emoji[:description].gsub('⊛', '').strip}";
       CASE
-    end.join("\n")
+    end.join("\n" + ' ' * 8)
   end
 end
 
-Generator.new(ARGV[0] || 'emoji-sequences.txt', ARGV[1] || 'Emoji.swift').main
+Generator.new(ARGV[0] || 'Emoji.swift').generate
